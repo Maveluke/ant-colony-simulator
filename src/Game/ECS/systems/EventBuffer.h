d87cd3b3a6a1fb0340c932900b345aa274fd11f4
Maveluke <maveluk.cy@gmail.com>
2026-01-18 12:19:29 -0500

    Implemented ECS event queue system that includes collision events

---
+++ src/Game/ECS/systems/EventBuffer.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "../Definition.h"
+#include <vector>
+#include <tuple>
+
+// Collision Events
+struct AntFoodCollision {
+  Entity ant;
+  Entity food;
+};
+
+struct AntColonyCollision {
+  Entity ant;
+  Entity colony;
+};
+
+struct AntSpiderCollision {
+  Entity ant;
+  Entity spider;
+};
+
+// Game Events (TODO: not used yet)
+struct EntityDeathEvent {
+  Entity entity;
+};
+
+struct FoodDepletedEvent {
+  Entity food;
+};
+
+// Event Buffer
+using EventBufferTuple = std::tuple<
+  std::vector<AntFoodCollision>,
+  std::vector<AntColonyCollision>,
+  std::vector<AntSpiderCollision>,
+  std::vector<EntityDeathEvent>,
+  std::vector<FoodDepletedEvent>
+>;
+
+class EventBuffer {
+private:
+  EventBufferTuple m_events;
+
+public:
+  EventBuffer() = default;
+
+  // Push an event of any registered type
+  template<typename T>
+  void Push(const T& event) {
+    std::get<std::vector<T>>(m_events).push_back(event);
+  }
+
+  // Get all events of a specific type
+  template<typename T>
+  const std::vector<T>& Get() const {
+    return std::get<std::vector<T>>(m_events);
+  }
+
+  void ClearAll() {
+    std::get<std::vector<AntFoodCollision>>(m_events).clear();
+    std::get<std::vector<AntColonyCollision>>(m_events).clear();
+    std::get<std::vector<AntSpiderCollision>>(m_events).clear();
+    std::get<std::vector<EntityDeathEvent>>(m_events).clear();
+    std::get<std::vector<FoodDepletedEvent>>(m_events).clear();
+  }
+};
